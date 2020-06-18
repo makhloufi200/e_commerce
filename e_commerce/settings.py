8ce7ae54977d3db2d29567c1c85ca26bc4fafa7e
makhloufi <makhloufi200@gmail.com>
2020-06-18 10:24:11 +0100

    add procfile and requirements file

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=hn9eka^&c*kf_1$)@0se6)bli_c-&(#e#$l_&clstqgcxju8b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
